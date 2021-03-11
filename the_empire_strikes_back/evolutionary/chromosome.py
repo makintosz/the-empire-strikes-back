fc407de6c93ff552abc7dfa374820dc12e0a5aa8
makintosz <grzegorz.kozinski34@gmail.com>
2021-03-11 23:19:21 +0100

    more initial stuff

---
+++ the_empire_strikes_back/evolutionary/chromosome.py
@@ -0,0 +1,6 @@
+class Chromosome:
+    def __init__(self):
+        self.weights = None
+
+    def generate(self):
+        pass
---
+++ the_empire_strikes_back/evolutionary/evolve.py
@@ -0,0 +1,6 @@
+from fitness_function.fitness_function import FitnessFunction
+
+
+def evolve() -> None:
+    """ Main evolutionary process. """
+    fitness = FitnessFunction()
